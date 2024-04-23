24188c2d335e5cd683272b22e29d0866d0ece0b8
Judith Quiñe <jqr@example.com>
2024-04-23 22:58:13 +0200

    first commit course js

---
+++ curso-javascript/hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// alert('hola');
+
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+
+alert("Es resultado de la operacion es:  "+operacion);
+
+
+// Tipos de datos
+var numero_entero = 44;
+var cadena_texto = "Hola 'que' tal";
+var verdadero_o_falso = false;
+
+var numero_falso =  "33";
+
+console.log(String(numero_falso)+4);
+console.log(numero_entero);
+console.log(cadena_texto);
+console.log(verdadero_o_falso);
+console.log(numero_falso);
---
+++ curso-javascript/hola-mundo/js/11-ejercicio1.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/*
+Programa que pida dos numeros y que nos diga cual es el mayor, el menor y si son iguales
+PLUS: si los numeros no son un numero o son menores o iguales a cero, nos los vuelva a pedir
+*/
+
+var numero1 = parseInt(prompt('Introduce el primer numero', 0));
+var numero2 = parseInt(prompt('Introduce el segundo numero', 0));
+
+
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)){
+	numero1 = parseInt(prompt('Introduce el primer numero', 0));
+	numero2 = parseInt(prompt('Introduce el segundo numero', 0));
+}
+
+console.log(numero1, numero2);
+
+if(numero1 == numero2){
+	alert("los numeros son iguales");
+
+}else if(numero1 > numero2){
+	alert("el numero mayor es: " + numero1);
+	alert("el numero menor es: " + numero2);
+
+}else if(numero2 > numero1){
+	alert("el numero mayor es: " + numero2);
+	alert("el numero menor es: " + numero1);
+
+}else{
+	alert("Introduce numeros correctos");
+}
