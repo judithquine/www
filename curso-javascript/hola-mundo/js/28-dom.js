e3d934a79b60dc3550bacee9b9e81271b8c5ab7b
judithquine <judithquine@yahoo.es>
2024-08-04 00:45:57 +0200

    dom

--- curso-javascript/hola-mundo/js/28-dom.js
+++ curso-javascript/hola-mundo/js/28-dom.js
@@ -2,74 +2,93 @@
 
 // DOM - Document Object Model
 
-function cambiarColor(color){
+
+//Ejecutar en la consola la funcion cambia color
+//cambiaColor("green");
+function cambiaColor(color) {
 	caja.style.background = color;
 }
 
-
-//var caja = document.getElementById('micaja').innerHTML;
-
-
 // Conseguir elementos con un ID concreto
 
-//var caja = document.getElementById('micaja');
-var caja = document.querySelector("#micaja");
+// var caja = document.getElementById('micaja').innerHTML;
+// 	var caja = document.getElementById("micaja");
+//
+// 	caja.innerHTML = "¡TEXTO EN LA CAJA DESDE JS!";
+// 	caja.style.background = "red";
+// 	caja.style.padding = "20px";
+// 	caja.style.color = "white";
+// 	caja.className = "hola hola2";
+// 	console.log(caja);
 
-caja.innerHTML = "¡TEXTO EN LA CAJA DESDE JS!";
-caja.style.background = "red";
-caja.style.padding = "20px";
-caja.style.color = "white";
-caja.class = "hola";
-caja.className = "hola hola2";
+// Seleccionar un elemento con querySelector
+	var caja = document.querySelector("#micaja");
 
+	caja.innerHTML = "¡TEXTO EN LA CAJA DESDE JS!";
+	caja.style.background = "red";
+	caja.style.padding = "20px";
+	caja.style.color = "white";
+	caja.className = "hola hola2";
+	//console.log(caja);
 
 // Conseguir elementos por su etiqueta
-var todosLosDivs = document.getElementsByTagName('div');
+	var todosLosDivs = document.getElementsByTagName('div');
+	//console.log(todosLosDivs);
 
-var seccion = document.querySelector("#miseccion");
-var hr = document.createElement("hr");
+// cambia contenido desde js
+	// 	var contenidoEnTexto = todosLosDivs[2];
+	//  console.log(contenidoEnTexto);
 
+	// 	var contenidoEnTexto = todosLosDivs[2];
+	//  contenidoEnTexto.innerHTML = "Otro texto para el segundo elemento";
+	//  console.log(contenidoEnTexto);
+	// contenidoEnTexto.style.background = "red";
 
+// no recomendable, forEach es para array con valores, mejor hacerlo con for
 //todosLosDivs.forEach((valor, indice) => {
-var valor;
-for(valor in todosLosDivs){
+//});
+	console.log(todosLosDivs);
 
-	if(typeof todosLosDivs[valor].textContent == 'string'){
+	var seccion = document.querySelector("#miseccion");
+	var hr = document.createElement("hr");
+
+
+ 	var valor;
+	for(valor in todosLosDivs) {
+		console.log(todosLosDivs[valor]);
+		if(todosLosDivs[valor].textContent.length >=1){
 		var parrafo = document.createElement("p");
 		var texto = document.createTextNode(todosLosDivs[valor].textContent);
 		parrafo.append(texto);
-		document.querySelector("#miseccion").append(parrafo);
+		parrafo.append(parrafo);
+		}
 	}
-}
-
-seccion.append(hr);
-
-
-// cambia contenido desde js
-//var contenidoEnTexto = todosLosDivs[2];
-//contenidoEnTexto.innerHTML = "Otro texto para el segundo elemento";
-//contenidoEnTexto.style.background = "red";
-//console.log(contenidoEnTexto);
-
+	seccion.append(hr);
 
 // Conseguir elementos por su clase css
-var divsRojos = document.getElementsByClassName("rojo");
-var divsAmarillos = document.getElementsByClassName("amarillo");
-divsAmarillos[0].style.background = "yellow";
+// var divsRojos = document.getElementsByClassName("rojo");
+// var divsAmarillos = document.getElementsByClassName("amarillo");
+// divsAmarillos[0].style.background = "yellow";
+//
 
-var div;
-for(div in divsRojos){
-	if(divsRojos[div].className == "rojo"){
-		divsRojos[div].style.background = "red";
-	}
-}
 
-// Query selector
-var id = document.querySelector("#encabezado");
-console.log(id);
 
-var claseRojo = document.querySelector(".rojo");
-console.log(claseRojo);
+//
+//
+//
+//
+//
+//
+//
+//
 
-var etiqueta = document.querySelector("div");
-console.log(etiqueta);
+
+// // Query selector
+// var id = document.querySelector("#encabezado");
+// console.log(id);
+//
+// var claseRojo = document.querySelector(".rojo");
+// console.log(claseRojo);
+//
+// var etiqueta = document.querySelector("div");
+// console.log(etiqueta);
